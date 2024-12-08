fdc4faeeaae668163d294b183d2c3e80c4913018
cyberthirst <cyberthirst.eth@gmail.com>
2024-12-08 15:39:13 +0100

    add decoder utils

---
+++ src/ivy/frontend/decoder_utils.py
@@ -0,0 +1,22 @@
+from vyper.semantics.types.primitives import _PrimT
+
+from ivy.types import StaticArray, DynamicArray, Map
+
+
+def decode_ivy_object(v, typ):
+    if isinstance(v, (StaticArray, DynamicArray)):
+        v = list(v)
+        if typ_needs_decode(typ.value_type):
+            v = [decode_ivy_object(x, typ.value_type) for x in v]
+    if isinstance(v, Map):
+        v = dict(v)
+        if typ_needs_decode(typ.value_type):
+            v = {k: decode_ivy_object(v, typ.value_type) for k, v in v.items()}
+    # TODO add struct and flag
+    return v
+
+
+def typ_needs_decode(typ):
+    if isinstance(typ, _PrimT):
+        return False
+    return True
