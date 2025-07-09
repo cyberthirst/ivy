20dabcd8638097bb9575bb86dfa59de80ea22a4c
cyberthirst <cyberthirst.eth@gmail.com>
2025-07-09 09:56:47 +0200

    decode ivy address objects for storage dumps

--- src/ivy/frontend/decoder_utils.py
+++ src/ivy/frontend/decoder_utils.py
@@ -1,6 +1,6 @@
 from vyper.semantics.types.primitives import _PrimT
 
-from ivy.types import StaticArray, DynamicArray, Map
+from ivy.types import StaticArray, DynamicArray, Map, Address
 
 
 def decode_ivy_object(v, typ):
@@ -9,10 +9,12 @@ def decode_ivy_object(v, typ):
         v = list(iter(v))
         if typ_needs_decode(typ.value_type):
             v = [decode_ivy_object(x, typ.value_type) for x in v]
-    if isinstance(v, Map):
+    elif isinstance(v, Map):
         v = dict(v)
         if typ_needs_decode(typ.value_type):
             v = {k: decode_ivy_object(v, typ.value_type) for k, v in v.items()}
+    elif isinstance(v, Address):
+        v = str(v)
     # TODO add struct and flag
     return v
 
